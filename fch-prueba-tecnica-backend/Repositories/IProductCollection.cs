e668726f1425ad7f0d60fbed013900f7b46c8e34
fchicaiza <fernando_chicaiza_g@hotmail.com>
2023-09-07 09:47:56 -0500

    Creación de métodos manejadores para el crud

---
+++ fch-prueba-tecnica-backend/Repositories/IProductCollection.cs
@@ -0,0 +1,13 @@
+﻿using fch_prueba_tecnica_backend.Models;
+
+namespace fch_prueba_tecnica_backend.Repositories
+{
+    public interface IProductCollection
+    {
+        Task InsertProduct(Product product);
+        Task UpdateProduct(Product product);
+        Task DeleteProduct(string id);
+        Task<List<Product>> GetAllProducts();
+        Task<Product> GetProductById(string id);
+    }
+}
---
+++ fch-prueba-tecnica-backend/Repositories/ProductCollection.cs
@@ -0,0 +1,50 @@
+﻿using fch_prueba_tecnica_backend.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.ObjectModel;
+
+namespace fch_prueba_tecnica_backend.Repositories
+{
+    public class ProductCollection : IProductCollection
+    {
+        internal MongoDBRepository _repository = new MongoDBRepository();
+        private IMongoCollection<Product> Collection;
+
+        public ProductCollection()
+        {
+            Collection = _repository.db.GetCollection<Product>("Products");
+        }
+
+        public async Task DeleteProduct(string id)
+        {
+            var filter = Builders<Product>.Filter.Eq(item => item.Id, new ObjectId(id));
+                await Collection.DeleteOneAsync(filter);
+        }
+
+        public  async Task<List<Product>> GetAllProducts()
+        {
+            return await Collection.FindAsync(new BsonDocument()).Result.ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(string id)
+        {
+            return await Collection.FindAsync(
+                new BsonDocument { { "_id",new ObjectId(id)} })
+                .Result
+                .FirstAsync();
+        }
+
+        public async Task InsertProduct(Product product)
+        {
+            await Collection.InsertOneAsync (product);
+        }
+
+        public async Task UpdateProduct(Product product)
+        {
+            var filter = Builders<Product>
+                .Filter
+                .Eq(item => item.Id, product.Id);
+            await Collection.ReplaceOneAsync(filter, product);
+        }
+    }
+}

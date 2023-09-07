52d9f89073d59d11325c8b963c387ac0ca7c8752
fchicaiza <fernando_chicaiza_g@hotmail.com>
2023-09-07 08:58:25 -0500

    Agregar archivos de proyecto.

---
+++ fch-prueba-tecnica-backend/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace fch_prueba_tecnica_backend.Models
+{
+    public class Product
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string prodCode { get; set; }
+
+        public string prodName { get; set; }
+
+        public string prodManofacturer { get; set; }
+
+        public string prodModel { get; set; }
+
+        public decimal price { get; set; }
+
+        public int stock { get; set; }
+    }
+}
